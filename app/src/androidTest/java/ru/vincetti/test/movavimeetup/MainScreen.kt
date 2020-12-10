3ca9906e76dcc5346d49dd2e008d7f194c3ebc3b
Mineev Oleg <o.mineev@movavi.com>
2020-12-10 23:36:13 +0700

    Screen 1

---
+++ app/src/androidTest/java/ru/vincetti/test/movavimeetup/MainScreen.kt
@@ -0,0 +1,10 @@
+package ru.vincetti.test.movavimeetup
+
+import com.kaspersky.kaspresso.screens.KScreen
+
+object MainScreen : KScreen<MainScreen>() {
+
+    override val layoutId: Int = R.layout.activity_main
+    override val viewClass: Class<*> = MainActivity::class.java
+
+}
