c745568bc7f91344fd9d8268395076fe052c2d7d
Mineev Oleg <o.mineev@movavi.com>
2020-12-13 19:55:43 +0700

    Test 1

---
+++ app/src/androidTest/java/ru/vincetti/test/movavimeetup/MeetUpTest.kt
@@ -0,0 +1,12 @@
+package ru.vincetti.test.movavimeetup
+
+import com.kaspersky.kaspresso.testcases.api.testcase.TestCase
+import org.junit.Test
+
+class MeetUpTest : TestCase() {
+
+    @Test
+    fun checkToastMessage_clickButton() {
+
+    }
+}
