a0d1a6096548aa5bff6b86e1296b844c7ab93919
Mineev Oleg <o.mineev@movavi.com>
2020-12-13 19:51:51 +0700

    Screen 2

--- app/src/androidTest/java/ru/vincetti/test/movavimeetup/MainScreen.kt
+++ app/src/androidTest/java/ru/vincetti/test/movavimeetup/MainScreen.kt
@@ -7,4 +7,7 @@ object MainScreen : KScreen<MainScreen>() {
     override val layoutId: Int = R.layout.activity_main
     override val viewClass: Class<*> = MainActivity::class.java
 
+    val showToastButton = KButton {
+        withId(R.id.main_activity_button)
+    }
 }
